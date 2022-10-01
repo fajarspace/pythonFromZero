918863dbf8d36b37b352eeb6de5fe5e605d4f575
Fajar Agung <fajarspace@Fajars-MacBook-Pro.local>
2022-10-01 21:33:36 +0700

    Add

---
+++ 1.castingTypeData.py
@@ -0,0 +1,3 @@
+data_int = 9
+data_str = str(data_int)
+print('ini adalah :',data_str,type(data_str))
---
+++ 2.inputUser.py
@@ -0,0 +1,10 @@
+data_str = input('masukkan kata :')
+print(data_str)
+
+data_int = int(input('Masukkan angka :'))
+print(data_int)
+
+data_bool = bool(int(input('Masukkan angka :')))
+print(data_bool)
+
+
