d6433059d10871717eedff535f919c8e19036578
Fajar Agung <fajarspace@Fajars-MacBook-Pro.local>
2022-10-02 11:52:39 +0700

    add program suhu

---
+++ 4.programKonversiCelcius.py
@@ -0,0 +1,15 @@
+print("\nPROGRAM KONVERSI CELSIUS\n")
+
+celcius = float(input('Masukkan suhu (dalam celsius) = '))
+
+#reamur
+reamur = (4/5) * celcius
+print('suhu dalam reamur adalah','=',reamur,'reamur')
+
+#fahrenheit
+fahrenheit = ((9/5) * celcius) + 32
+print('suhu dalam fahrenheit adalah','=',fahrenheit,'fahrenheit')
+
+#kelvin
+kelvin = celcius + 273
+print('suhu dalam kelvin adalah','=',kelvin,'kelvin')
